c12eccb8ae25fb519dbb85bee37f19eb3f3ca74f
JAROT OFC <108053350+Alpiii22@users.noreply.github.com>
2022-09-09 18:28:29 +0700

    Create setbio.js

---
+++ plugins/setbio.js
@@ -0,0 +1,17 @@
+// By Jarot
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `Harap masukan text!\n\nCotoh: ${usedPrefix + command} Oscar Bot`
+  try {
+    await conn.setStatus(text)
+    m.reply('Berhasil!')
+  } catch (e) {
+    console.log(e)
+    throw `Eror`
+  }
+}
+handler.help = ['setbiobot <teks>']
+handler.tags = ['owner']
+handler.command = /^(setbiobot)$/i
+handler.owner = true
+
+module.exports = handler
